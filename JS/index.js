3cd45e841edf19c9635b25caad6b806615ec1e37
kunal jethva <kunaljethva90@gmail.com>
2023-12-01 13:06:00 +0530

    removed landing page

--- JS/index.js
+++ JS/index.js
@@ -2,7 +2,6 @@ let hambuger = document.getElementById('hamburger');
 let cross = document.getElementById('cross');
 let mobNav = document.getElementById('movnav');
 let allMobNav = document.querySelectorAll('.all-mob-nav');
-let landHeader = document.getElementById('landing-menu');
 
 hambuger.addEventListener('click', () => {
     mobNav.classList.toggle("nav-active");
@@ -10,7 +9,6 @@ hambuger.addEventListener('click', () => {
     allMobNav.forEach(function (e) {
         e.classList.toggle('d-none')
     });
-    landHeader.classList.add('fixed');
 })
 
 cross.addEventListener('click', () => {
@@ -19,8 +17,4 @@ cross.addEventListener('click', () => {
     allMobNav.forEach(function (e) {
         e.classList.toggle('d-none')
     });
-    landHeader.classList.add('fixed');
-    setTimeout(()=>{
-        landHeader.classList.remove('fixed');
-    },500);
 })
